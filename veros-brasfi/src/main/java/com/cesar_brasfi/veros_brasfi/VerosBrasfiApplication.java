930dccbb87a2b9af2342337749d29823ae8c3868
Antônio José Paes de Andrade Júnior Paes <123177984+AntonioPaess@users.noreply.github.com>
2025-02-21 11:37:26 -0300

    chore: inicializa projeto Spring Boot
    - Configuração inicial do projeto utilizando Spring Boot.
    - Estrutura básica gerada contendo:
      - Estrutura de diretórios padrão (src/main/java, src/test/java).
      - Configuração de build com Maven/Gradle.
      - Dependências iniciais definidas no arquivo de configuração (application.properties, pom.xml ou build.gradle).
      - Ponto de entrada principal com a classe anotada com @SpringBootApplication.

---
+++ veros-brasfi/src/main/java/com/cesar_brasfi/veros_brasfi/VerosBrasfiApplication.java
@@ -0,0 +1,13 @@
+package com.cesar_brasfi.veros_brasfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerosBrasfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerosBrasfiApplication.class, args);
+	}
+
+}
---
+++ veros-brasfi/src/test/java/com/cesar_brasfi/veros_brasfi/VerosBrasfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesar_brasfi.veros_brasfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerosBrasfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
