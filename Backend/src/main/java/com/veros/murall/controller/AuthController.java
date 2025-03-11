0588005a29cc8f24b43f6938e57a32dbdab5918d
vinivent <vini.ventura999@gmail.com>
2025-03-11 11:45:40 -0300

    feat: simplificando resposta do post register no auth controller

--- Backend/src/main/java/com/veros/murall/controller/AuthController.java
+++ Backend/src/main/java/com/veros/murall/controller/AuthController.java
@@ -1,25 +1,19 @@
 package com.veros.murall.controller;
 
 import com.veros.murall.util.JwtUtil;
-import com.veros.murall.model.user.User;
 import com.veros.murall.model.user.dto.LoginRequest;
 import com.veros.murall.model.user.dto.RegisterRequest;
-import com.veros.murall.model.user.dto.UserResponse;
 import com.veros.murall.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -46,9 +40,8 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
-        User user = userService.createUser(request);
-        UserResponse userResponse = new UserResponse(user.getId(), user.getUsername(), user.getEmail(), user.getRole());
+        userService.createUser(request);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Usuário criado com sucesso: " + userResponse);
+                .body("Usuário cadastrado com sucesso!");
     }
 }
