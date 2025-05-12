0bdd415b88c3e0116d24e5a75044c1041298866b
AntonioPaess <paesdeandradeantonio@gmail.com>
2025-05-12 19:51:38 -0300

    feat: remoção da dependência dotenv-java e ajustes no Dockerfile para variáveis de ambiente

--- Backend/src/main/java/com/veros/murall/MurallApplication.java
+++ Backend/src/main/java/com/veros/murall/MurallApplication.java
@@ -1,6 +1,5 @@
 package com.veros.murall;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MurallApplication {
 
     public static void main(String[] args) {
-        // Carrega as variáveis do arquivo .env e as define como propriedades do sistema
-        Dotenv.configure().directory(".").load()
-            .entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
-        // Inicia a aplicação Spring Boot
         SpringApplication.run(MurallApplication.class, args);
     }
 }
